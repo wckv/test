3cb41ee4cd876ba36fa6f196b0d2622b0ea9df57
wckv <115908669+wckv@users.noreply.github.com>
2024-03-02 05:56:11 +0800

    Delete 1.php

--- 1.php
+++
@@ -1,8 +0,0 @@
-<?php
-// 這是一個註解，它不會被執行
-// echo 是一個 PHP 的函式，它會將字串輸出到網頁上
-echo "Hello World<br>"; // <br> 是一個 HTML 的標籤，它會換行
-// date 是一個 PHP 的函式，它會回傳當前的日期和時間，你可以指定不同的格式
-echo "Today is " . date("Y/m/d") . "<br>"; // . 是一個 PHP 的運算子，它會連接字串
-echo "The time is " . date("h:i:sa");
-?>
